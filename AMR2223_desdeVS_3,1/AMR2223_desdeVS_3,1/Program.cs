755ba816c6ba73d50aed31955c0dcf822e85636b
AMR-2223 <albmesrod@alu.edu.gva.es>
2022-11-14 09:31:31 +0100

    prueba en main

---
+++ AMR2223_desdeVS_3,1/AMR2223_desdeVS_3,1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AMR2223_desdeVS_3_1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
