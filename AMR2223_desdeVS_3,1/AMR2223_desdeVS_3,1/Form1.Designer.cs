0add6f1d7e7c7c9e91f614eff928dfee41fbab27
AMR-2223 <albmesrod@alu.edu.gva.es>
2022-11-15 10:16:09 +0100

    ejecutado

--- AMR2223_desdeVS_3,1/AMR2223_desdeVS_3,1/Form1.Designer.cs
+++ AMR2223_desdeVS_3,1/AMR2223_desdeVS_3,1/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbUrgente = new System.Windows.Forms.CheckBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTelegrama = new System.Windows.Forms.TextBox();
+            this.Calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Texto";
+            // 
+            // cbUrgente
+            // 
+            this.cbUrgente.AutoSize = true;
+            this.cbUrgente.Location = new System.Drawing.Point(81, 315);
+            this.cbUrgente.Name = "cbUrgente";
+            this.cbUrgente.Size = new System.Drawing.Size(102, 24);
+            this.cbUrgente.TabIndex = 1;
+            this.cbUrgente.Text = "Urgente?";
+            this.cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(173, 369);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 26);
+            this.txtPrecio.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 372);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Coste:";
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Location = new System.Drawing.Point(65, 81);
+            this.txtTelegrama.Multiline = true;
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(433, 228);
+            this.txtTelegrama.TabIndex = 4;
+            // 
+            // Calcular
+            // 
+            this.Calcular.Location = new System.Drawing.Point(386, 348);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(112, 69);
+            this.Calcular.TabIndex = 5;
+            this.Calcular.Text = "button1";
+            this.Calcular.UseVisualStyleBackColor = true;
+            this.Calcular.Click += new System.EventHandler(this.Calcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.txtTelegrama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.cbUrgente);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbUrgente;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTelegrama;
+        private System.Windows.Forms.Button Calcular;
     }
 }
 
